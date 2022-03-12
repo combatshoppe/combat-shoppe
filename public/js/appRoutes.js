70c869494d876369aad996851dce059ac4031bd4
Dru Ellering <54593927+dru26@users.noreply.github.com>
2022-03-12 15:58:50 -0500

    Added Position class
    Added the Position class to data-utils.js and created data-utils.js

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -3,20 +3,20 @@ angular.module('appRoutes', []).config(['$routeProvider',
     function($routeProvider, $locationProvider) {
         $routeProvider
         // home page
-            .when('/', {
-                templateUrl: 'views/home.html',
-                controller: 'MainController'
-            })
-            // simulator page that will use the SimulatorController
-            .when('/simulator', {
-                templateUrl: 'views/simulator.html',
-                controller: 'StudentController'
-            })
-            // homebrew page that will use the HomebrewController
-            .when('/homebrewBuilder', {
-                templateUrl: 'views/homebrewBuilder.html',
-                controller: 'HomebrewController'
-            });
+          .when('/', {
+              templateUrl: 'views/home.html',
+              controller: 'MainController'
+          })
+          // simulator page that will use the SimulatorController
+          .when('/simulator', {
+              templateUrl: 'views/simulator.html',
+              controller: 'StudentController'
+          })
+          // homebrew page that will use the HomebrewController
+          .when('/homebrewBuilder', {
+              templateUrl: 'views/homebrewBuilder.html',
+              controller: 'HomebrewController'
+          });
         $locationProvider.html5Mode(true);
     }
 ]);
---
+++ public/js/data-utils.js
@@ -0,0 +1,16 @@
+/*
+data-utils.js
+A file for all enums and extremely basic shared classes that many files can
+import.
+*/
+
+// Position class
+class Position {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+// export allows us to pass these to other files when it is imported
+export { Position };
