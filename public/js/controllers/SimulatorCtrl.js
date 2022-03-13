7f8fc6c68631b8dd535551490f2b14bf28e8cb38
Dru Ellering <54593927+dru26@users.noreply.github.com>
2022-03-13 19:48:35 -0400

    Fixed layout and made color blocks
    Added new CSS styles, blocked off the navigation, and selected the DOM elements needed to create Windows

--- public/js/controllers/SimulatorCtrl.js
+++ public/js/controllers/SimulatorCtrl.js
@@ -1,10 +1,7 @@
 angular.module('SimulatorCtrl', ['WindowModule']).controller('SimulatorController', function($scope, $window) {
-	//console.log(injector.get('jsInterface'))
 	$scope.tagline = 'Welcome to Simulator section!';
-
-	//import Player from "./../data-utils.js";
-	let p = new Position(1,0);
-	//alert("Angularjs call function on page load");
+	let mainWindow = document.querySelectorAll('[name="MainWindow"]');
+	let mainWindow = document.querySelectorAll('[name="SideWindow"]');
 });
 
 function Edit() {
