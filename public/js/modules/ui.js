195be6a0b800efeb339aa14d53929eeacae4c467
Dru Ellering <54593927+dru26@users.noreply.github.com>
2022-03-18 22:27:45 -0400

    Updated ElementHTML class
    Updated the class constructor so it could actually be useful

--- public/js/modules/ui.js
+++ public/js/modules/ui.js
@@ -9,44 +9,76 @@
 /** Class representing a point. */
 class ElementHTML {
 	/**
-	 * 'Virtual' constructor
+	 * Default constructor
+	 * @param {Position} offset - The place to put the element relative to parent
+	 * @param {int} width - Width of the DOM
+	 * @param {int} height - Height of the DOM
+	 * @param {DOM} parent - The DOM of the parent
 	 * @constructor
 	 */
-	constructor() {
+	constructor(offset, width, height, parent = null) {
 		this.dom = null;
+		create(offset, width, height, parent);
 	}
 
 	/**
 	 * An inherited function that should be called in the constructor
-	 * @param {Position} position - The place to put the element relative to parent
-	 * @param {string} element - The type of element to make
+	 * @param {Position} offset - The place to put the element relative to parent
 	 * @param {DOM} parent - The DOM of the parent
+	 * @param {int} width - Width of the DOM
+	 * @param {int} height - Height of the DOM
+	 * @param {string} element - The type of element to make
 	 */
-	create(position, element = 'div', parent = null) {
-		// Error checking
-		if (this.dom === null) return;
+	create(offset, parent, width, height, element = 'div') {
 		// Create the DOM
 		this.dom = document.createElement(element);
+		// Add the parent to the child
+		if (parent != null) parent.appendChild(this.dom);
 		// The element is positioned relative to its first positioned (not static)
 		// ancestor element. Find details here:
 		// https://www.w3schools.com/cssref/pr_class_position.asp
 		// https://www.javascripttutorial.net/javascript-dom/javascript-style/
 		this.dom.style.position = 'absolute';
-		// Set the actual position
-		this.dom.style.left = position.x.toString() + 'px';
-		this.dom.style.top = position.y.toString() + 'px';
+		// Set the actual position and size
+		this.dom.style.left = offset.x.toString() + 'px';
+		this.dom.style.top = offset.y.toString() + 'px';
+		this.dom.style.width = width.toString() + 'px';
+		this.dom.style.height = height.toString() + 'px';
 	}
 
 	/**
 	 * Moves the DOM by a certain amount
 	 * @param {Position} delta - The amount to move by
+	 * @returns {Position} The current position the DOM is at
 	 */
 	move(delta) {
 		// Error checking
 		if (this.dom == null) return;
+		// Calculate the new position
+		let newPosition = new Position(parseInt(this.dom.style.left) + delta.x,
+		                               parseInt(this.dom.style.top) + delta.y)
 		// Actually move the element
-		this.dom.style.left = (parseInt(this.dom.style.left) + delta.x).toString() + 'px';
-		this.dom.style.top = (parseInt(this.dom.style.top) + delta.y).toString() + 'px';
+		this.dom.style.left = newPosition.x.toString() + 'px';
+		this.dom.style.top = newPosition.y.toString() + 'px';
+		// Return the position
+		return newPosition;
+	}
+
+	/**
+	 * Moves the DOM by a certain amount and deletes if out of bounds of parent
+	 * (0,0) to (width, height)
+	 * @param {Position} delta - The amount to move by
+	 * @returns {Boolean} True if the element was deleted
+	 */
+	moveAndDelete(delta) {
+		let newPosition = this.move(delta);
+		if (this.dom.parentNode == null) return false;
+		let upperLeft = new Position(newPosition.x, newPosition.y);
+
+
+		//
+		let lowerRight = new Position(newPosition.x, newPosition.y);
+
 	}
 
 	/**
@@ -58,10 +90,10 @@ class ElementHTML {
 		// Error checking
 		if (this.dom == null) return;
 		// Check if outside
-		if (position.x > this.this.dom.style.width) return false;
-		if (position.y > this.this.dom.style.height) return false;
-		if (position.x < this.this.dom.style.left) return false;
-		if (position.y < this.this.dom.style.top) return false;
+		if (position.x > this.dom.style.width) return false;
+		if (position.y > this.dom.style.height) return false;
+		if (position.x < this.dom.style.left) return false;
+		if (position.y < this.dom.style.top) return false;
 		// If not outside, must be inside
 		return true;
 	}
