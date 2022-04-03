d6ae12e3744af6cdcfe266f5e9da0a97d406f903
connorboucher <connorwboucher2015@gmail.com>
2022-04-02 20:11:39 -0400

    Updated comments and sped up pathfinding

--- public/js/modules/simulator.js
+++ public/js/modules/simulator.js
@@ -7,20 +7,20 @@
  var WindowModule = angular.module('SimulatorModule', ['DataModule'])
 
 /**
- * The main class behind the Simulation. It handles pathfinding, running rounds of combat, and updating the displays.
+ * The main class behind the Simulation. It handles pathfinding, running rounds of combat, and updating the displays
  */
 class Simulator {
 	/**
 	 * Creates a simulation for the grid
 	 * @constructor
-	 * @param {GridDisplay} initialDisplay - 2-dimensional matrix of Tile spaces
+	 * @param {GridDisplay} initialDisplay - display of the grid
 	 * @param {Delta[][]} deltas - a log of turn deltas, sorted in 1 dimension, where delta[0] corresponds to turn 1
 	 */
-	 constructor(initialDisplay, deltas) {
+	 constructor(initialDisplay, deltas = null) {
 	 	// Init variables
 	 	this.copyGrid = initialDisplay.grid; // This should be a copy, currently pass by ref
 	 	this.display = initialDisplay;
-	 	this.deltas = deltas;
+	 	this.deltas = deltas; // Currently, deltas are unimplemented
 	 }
 
 	/**
@@ -30,11 +30,11 @@ class Simulator {
 		this.currentDisplay = this.initialDisplay;
 
 		// may need to add more so everything is reset correctly
-		// for example: might need an int to keep track of current turn
+		// for example: might need to keep track of current turn and reset that
 	}
 
 	/*
-	 *
+	 * Updates the values stored on the displayed grid and calls a redraw on the display after changes
 	 */
 	updateDisplay() {
 		this.display.grid = this.copyGrid;
@@ -43,14 +43,14 @@ class Simulator {
 
 	/**
 	 * Run through the entire simulation until it ends
-	 * @param {GridDisplay} display - display of the grid
+	 * @param {GridDisplay} gridDisplay - display of the grid
 	 * @param {Token[]} initiative - an ordered list of Token objects
 	 */
-	async run(display, initiative) {
+	async run(gridDisplay, initiative) {
 		let turnLimit = 30;
 		for (let turn = 1; turn < turnLimit; turn++) {
 
-			// Check for stalling edge case, currently just have turn limit
+			// Maybe check for stalling edge case, currently just have turn limit
 
 			// Check if simulation is complete
 			if (simulationComplete()) {
@@ -67,6 +67,8 @@ class Simulator {
 
 	/**
 	 * Run the simulation through one turn of combat and update displays
+	 * @param {GridDisplay} gridDisplay - display of the grid
+	 * @param {Token[]} initiative - an ordered list of Token objects
 	 */
 	stepForward(gridDisplay, initiative) {
 		throw 'Simulator.stepForward is not defined!';
@@ -89,28 +91,30 @@ class Simulator {
 
 	/**
 	 * Run the simulation through one turn of combat without updating displays
+	 * @param {Token[]} initiative - an ordered list of Token objects
 	 */
 	async _forward(initiative) {
-		// throw 'Simulator._forward is not defined!';
 
-		// Loop through ordered list of Tokens
+		// Loop through ordered list of Token objects
 		for (let i = 0; i < initiative.length; i++) {
 
-			// Look at current Token on grid
+			// Look at current token on grid
 			let token = initiative[i];
 
+			// Loop until all actions of the token have resolved
 			let done = false;
-			console.log("Begin Do Function:");
 			while (!done) {
 
+				// Init start and goal positions as location of current token
 				let currentPosition = new Position(token.row, token.column);
 				let moveTo = new Position(token.row, token.column);
 
+				// Find where the token should move to and what it should do
 				done = token.behavior.do(moveTo, initiative, token);
 
+				// Find a path to the destination
 				let positions = this._pathfind(this.copyGrid, currentPosition, moveTo);
 
-				//let pos = moveTo;
 				for (let i = 1; i < positions.length; ++i) {
 					//pos = new Position(positions[positions.length-1].x, positions[positions.length-1].y);
 					if (positions[i].x !== token.row || positions[i].y !== token.column) {
@@ -140,6 +144,7 @@ class Simulator {
 
 	/**
 	 * Finds a path on the grid to a given destination tile
+	 * @param {Grid} grid - 2-dimensional matrix of Tile spaces
 	 * @param {Position} start - the starting location
 	 * @param {Position} goal - the destination location
 	 * @returns {Position[]}
@@ -219,34 +224,36 @@ class Simulator {
 				let weight = 1;
 
 				// If neighbor is is openList
-				let openNode = null;
+				let inOpenList = false;
 				openList.forEach((node) => {
 					if (neighbor.equals(node.position)) {
 						// neighbor.parent = currentNode;
 						if (currentNode.g + weight <= neighbor.g) {
 
-							openNode = node;
+							inOpenList = true;
 							return;
 						}
 					}
 				});
 
 				// If neighbor is in closedList
-				let closedNode = false;
+				let inClosedList = false;
 				closedList.forEach((node) => {
 					if (neighbor.equals(node.position)) {
 						// neighbor.parent = currentNode;
 						if (currentNode.g + weight <= neighbor.g) {
 
-							closedNode = node;
+							inClosedList = true;
 							return;
 						}
 					}
 				});
 
 				// If not in open and closed lists, add node to open list
-				let newNode = new Node(new Position(neighbor.x, neighbor.y), neighbor.g, neighbor.h, currentNode);
-				openList.push(newNode);
+				if (!inOpenList && !inClosedList) {
+					let newNode = new Node(new Position(neighbor.x, neighbor.y), neighbor.g, neighbor.h, currentNode);
+					openList.push(newNode);
+				}
 			});
 		}
 	}
@@ -298,7 +305,7 @@ class Simulator {
 }
 
 /**
- * A helper which holds a Position and several heuristics for pathfinding purposes.
+ * A helper class which holds a Position and several heuristics for pathfinding purposes
  */
 class Node {
 	/**
@@ -314,7 +321,7 @@ class Node {
 	 	this.g = g;
 		this.h = h;
 		this.f = g + h;
-		this.parent = parent; // new Position(-1, -1);
+		this.parent = parent;
 	}
 
 	/**
